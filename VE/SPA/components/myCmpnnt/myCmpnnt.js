c2f04e89e4ac33d2ce56186028ca2d1bbd8801b3
Condor <condorrr@gmail.com>
2018-04-29 12:40:46 +0300

    start

---
+++ VE/SPA/components/myCmpnnt/myCmpnnt.js
@@ -0,0 +1,10 @@
+﻿Vue.component('myCmpnnt', {
+    data: function () {
+        return {
+            count: 0
+        }
+    },
+    template: app.services("TemplateService").Get("components/myCmpnnt/myCmpnnt.html")
+
+
+})
---
+++ VE/SPA/services/app.js
@@ -0,0 +1,48 @@
+﻿(function () {
+    var _servicesFn = {};
+    var _servicesItems = {};
+    var _injections = {};
+    if (!window.app) {
+        window.app = {};
+    }
+    if (!window.app.services) {
+        window.app.services = function (name, fn, injections) {
+            if (fn) {
+                if (_servicesFn[name]) {
+                    console.log("App error: creating service '" + name + "' is exists, already!!!");
+                    throw new Error("App error: creating service '" + name + "' is exists, already!!!");
+                    return null;
+                }
+                _servicesFn[name] = fn;
+                if (injections) {
+                    _injections[name] = injections;
+                }
+                return window.app;
+            } else {
+                if (!_servicesFn[name]) {
+                    console.log("App error: call undefined service '" + name + "'!!!");
+                    throw new Error("App error: call undefined service '" + name + "'!!!");
+                    return null;
+                }
+                if (!_servicesItems[name]) {
+                    var args = [];
+                    if (_injections[name]) {
+                        for (var i = 0; i < _injections[name].length; i++) {
+                            var injectionName = _injections[name][i];
+                            var injection = window.app.services(injectionName);
+                            if (injection) {
+                                args.push(injection);
+                            }
+
+                        }
+                    }
+                    _servicesItems[name] = _servicesFn[name].apply({}, args);
+                }
+                return _servicesItems[name];
+            }
+        }
+    }
+
+
+
+})();
